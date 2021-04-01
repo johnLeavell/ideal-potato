0411d5e4ba6a51e5f3a9d7d74c3b7f668334bda1
John Leavell <johnleavell@gmail.com>
2021-03-31 22:47:24 -0500

    building out table data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -10,7 +11,7 @@ const App = () => {
       <Header/>
       <main className='py-3'>
         <Container>
-          {/* <Route path='/' component={HomeScreen} exact /> */}
+          <HomeScreen />
         </Container>
       </main>
       <Footer/>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import products from '../products.js';
 import { Row, Col } from 'react-bootstrap';
-
+import Product from '../components/Product';
 
 const HomeScreen = () => {
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <>
+          <h1>Imperfect Supply List</h1>
+          <Row>
+            {products.map((product) => (
+              <Col xs={6} sm={12} md={4} lg={4} lx={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      );
+};
 
 export default HomeScreen;
