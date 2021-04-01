ca728c9a25b5ab41769029ccc8b1a20d9e598b88
John Leavell <johnleavell@gmail.com>
2021-04-01 13:41:45 -0500

    fixed routes for cart, sign in, and home.  Added product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import products from './products';
 
 const App = () => {
   return (
@@ -11,7 +13,8 @@ const App = () => {
       <Header/>
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer/>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,62 +1,70 @@
 import React from 'react';
-import { Table, Container } from 'react-bootstrap';
+import { Container, Card, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
   console.log(product);
   return (
     <Container>
-        <Table responsive='sm'>
-      <thead>
-       <tr>
-         <th>bank name</th>
-         <th>card id</th>
-         <th>Date opened</th>
-         <th>Credit Limit</th>
-         <th>Cards Available</th>
-       </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{product.bankName}</td>
-          <td>{product.cardId}</td>
-          <td>{product.dateOpened}</td>
-          <td>{product.creditLimit}</td>
-          <td>{product.availability > 0 ? 'Available' : 'None Available'}</td>
-        </tr>
-      </tbody>
-      </Table>
+      <Card className="my-1 p-1 rounded">
+
+        <Link to={`/product/${product._id}`}>
+          <Image src={product.image} variant="top" thumbnail fluid />
+        </Link>
+
+        <Card.Body>
+          <Link to={`/product/${product._id}`}>
+          <Card.Title as="h4">
+            <strong className='text-primary'>{product.bankName}</strong>
+          </Card.Title>
+          </Link>
+          
+          <Card.Text as="div">
+            <div className="my-1">Card ID: {product.cardId}</div>
+          </Card.Text>
+
+          <Card.Text as="div">
+            <div className="my-1">Date Opened: {product.dateOpened}</div>
+          </Card.Text>
+
+          <Card.Text as="h5">
+            <div className="my-1">Credit Limit: ${product.creditLimit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+          </Card.Text>
+
+          <Card.Text as="div">
+            <div className="my-1">{product.availability} {product.availability > 0 ? 'Available' : 'None Available'}</div>
+          </Card.Text>
+
+        </Card.Body>
+      </Card>
     </Container>
  
   );
 };
 
 export default Product;
-{/* <Card className="my-1 p-1 rounded">
-<Link to={`/product/${product._id}`}>
-  <Image src={product.image} variant="top" thumbnail fluid />
-</Link>
-<Card.Body>
-    <Card.Title as="h4">
-      <strong className='text-primary'>{product.bankName}</strong>
-    </Card.Title>
 
-  <Card.Text as="div">
-    <div className="my-1">Card ID: {product.cardId}</div>
-  </Card.Text>
 
-  <Card.Text as="div">
-    <div className="my-1">Date Opened: {product.dateOpened}</div>
-  </Card.Text>
 
-  <Card.Text as="h5">
-    <div className="my-1">Credit Limit: ${product.creditLimit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
-  </Card.Text>
 
-  <Card.Text as="div">
-    <div className="my-1">{product.availability} {product.availability > 0 ? 'Available' : 'None Available'}</div>
-  </Card.Text>
 
-</Card.Body>
-
-</Card> */}
+{/* <Table responsive='sm'>
+<thead>
+ <tr>
+   <th>bank name</th>
+   <th>card id</th>
+   <th>Date opened</th>
+   <th>Credit Limit</th>
+   <th>Cards Available</th>
+ </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>{product.bankName}</td>
+    <td>{product.cardId}</td>
+    <td>{product.dateOpened}</td>
+    <td>{product.creditLimit}</td>
+    <td>{product.availability > 0 ? 'Available' : 'None Available'}</td>
+  </tr>
+</tbody>
+</Table> */}
