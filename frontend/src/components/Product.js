d8842a737567fec548ca47179ce5c0b2ae510a0b
John Leavell <johnleavell@gmail.com>
2021-04-01 21:23:52 -0500

    finshed individual products screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Card, Image } from 'react-bootstrap';
+import { Container, Card, Image, } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 
@@ -11,38 +11,39 @@ const Product = ({ product }) => {
 
   return (
     <Container>
-    <Card className="my-1 p-1 rounded">
+      <Card className="my-1 p-1 rounded">
+        <Link to={`/product/${product._id}`}>
+          <Image src={product.image} variant="top" thumbnail fluid />
+        </Link>
+        
+        <Card.Body>
+          <Card.Title as="h4">
+            <strong className='text-primary'>{product.bankName}</strong>
+          </Card.Title>
+          
+          <Card.Text as="div">
+            <div className="my-1">Card ID: {product.cardId}</div>
+          </Card.Text>
 
+          <Card.Text as="div">
+            <div className="my-1">Date Opened: {product.dateOpened}</div>
+          </Card.Text>
 
-  <Image src={product.image} variant="top"  />
+          <Card.Text as="h5">
+            <div className="my-1">Credit Limit: ${product.creditLimit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+          </Card.Text>
 
-
-  <Card.Body>
-
-    <Card.Title as="h4">
-      <strong className='text-primary'>{product.bankName}</strong>
-    </Card.Title>
-
-    
-    <Card.Text as="div">
-      <div className="my-1">Card ID: {product.cardId}</div>
-    </Card.Text>
-
-    <Card.Text as="div">
-      <div className="my-1">Date Opened: {product.dateOpened}</div>
-    </Card.Text>
-
-    <Card.Text as="h5">
-      <div className="my-1">Credit Limit: ${product.creditLimit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
-    </Card.Text>
-
-    <Card.Text as="div">
-      <div className="my-1">{product.availability} {product.availability > 0 ? 'Available' : 'None Available'}</div>
-    </Card.Text>
-
-  </Card.Body>
-</Card>
-</Container> 
+          <Card.Text as="div">
+            <div className="my-1">{product.availability} {product.availability > 0 ? 'Available' : 'None Available'}</div>
+          </Card.Text>
+          
+          <Card.Text as='h5'>
+            <div className='my-1'>Price: ${product.price.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}
+            </div>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container> 
   );
 };
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -22,9 +22,41 @@ const ProductScreen = ({ match }) => {
                         <h3>{product.bankName}</h3>
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        
+                        Price: ${product.price.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}
                     </ListGroup.Item>
                 </ListGroup>
+            </Col>
+            <Col md={3}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>
+                                    Price:
+                                </Col>
+                                <Col>
+                                    <strong>${product.price.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>
+                                    Status:
+                                </Col>
+                                <Col>
+                                    {product.availability > 0 ? 'Available' : 'None Available'}
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button className='btn-block' type='button' disabled={product.availability === 0 }>
+                                Add To Cart
+                            </Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
             </Col>
         </Row>
        </>
